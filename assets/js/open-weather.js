a2a291635fd3df28e4afd121dd568de076b0a917
Patrick O'Dea <po39dea@gmail.com>
2023-08-03 23:23:33 -0600

    Added geocode lat and lon api

--- assets/js/open-weather.js
+++ assets/js/open-weather.js
@@ -1,27 +1,43 @@
-var ApiKey = "6257fbb45c08f20e1cc17462b2450f53";
 var cityInput = document.getElementById("formInput");
 var button = document.getElementById("button");
 var output = document.getElementById("outputApi");
+var nameWeather = document.getElementById("name");
+var temp = document.getElementById("temp");
+var wind = document.getElementById("wind");
+var humidity = document.getElementById("humidity");
 
 function getApi(){
     var city = cityInput.value;
-    var geocoding= "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + ApiKey;
+    var geocoding= "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=6257fbb45c08f20e1cc17462b2450f53";
 
     fetch(geocoding)
-        .then(function (response) {
-            if (!response){
-                alert("Failed Input");
-                return;
-            }
-            return response.json();
-                    
+    .then(function (response) {
+        return response.json();
     })
     
-        .then(function(data){
-            output.innerHTML = data[0].lat + data[0].lon;
-        })
+    .then(function (data) {
+        console.log(data)
 
-};
+        var lat = response.data.city.coord.lat;
+        var lon = response.data.city.coord.lon;
+
+    fetch ("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=6257fbb45c08f20e1cc17462b2450f53");  
+    .then(function (weatherResponse) {
+        return weatherResponse.json();
+    })
+    .then(function (weatherData) {
+        console.log(weatherData);
+
+        nameWeather.textContent = weatherData.name;
+        temp.textContent = weatherData.main.temp;
+        wind.textContent = weatherData.wind.speed;
+        humidity.textContent = weatherData.main.humidity;
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+});
+}
 
 
 button.addEventListener("click", getApi);
